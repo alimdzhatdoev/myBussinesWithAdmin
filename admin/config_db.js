18c4b75e344ae2f8616d0a9fc2735f8479483519
alimdzhatdoev <alimdzhatdoev1gmail.com>
2023-10-31 14:11:00 +0300

    ADD SLIDER

--- admin/config_db.js
+++ admin/config_db.js
@@ -1,5 +1,32 @@
 export function schema() {
     return ({
+        "slider": {
+            "menuName": "Слайды",
+            "title": {
+                "name": "Название слайда",
+                "element": "input",
+                "type": "text",
+                "required": true
+            },
+            "text": {
+                "name": "Текст слайда",
+                "element": "textarea",
+                "type": "text",
+                "required": true
+            },
+            "link": {
+                "name": "Ссылка для кнопки",
+                "element": "input",
+                "type": "text",
+                "required": true
+            },
+            "img": {
+                "name": "Картинки слайда",
+                "element": "input",
+                "type": "file",
+                "required": true
+            },
+        },
         "news": {
             "menuName": "Новости",
             "title": {
@@ -26,6 +53,6 @@ export function schema() {
                 "type": "file",
                 "required": true
             },
-        }
+        },
     })
 }
